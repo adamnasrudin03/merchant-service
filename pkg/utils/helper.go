3e7dec91032506b155971973d58d1f61cff6e25c
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-12-02 10:56:53 +0700

    helper date between

--- pkg/utils/helper.go
+++ pkg/utils/helper.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"log"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -13,4 +14,19 @@ func HashPassword(password string) string {
 		panic("Failed to hash a password")
 	}
 	return string(bytes)
+}
+
+func DateBetween(start, end time.Time) ([]string, []time.Time) {
+	resString := []string{}
+	resTime := []time.Time{}
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		resTime = append(resTime, d)
+		resString = append(resString, d.Format("2006-01-02"))
+	}
+
+	return resString, resTime
+}
+
+func Date(year, month, day int) time.Time {
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 }
